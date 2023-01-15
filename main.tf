998944c7ab3a655b9e273e1b69979c3a0729c0cb
Jamison Blanchard <jamison.p.blanchard@gmail.com>
2023-01-14 16:02:54 -0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+locals {
+  # Side step the leading and following slash issue by removing the provided
+  # ones and adding them in the format the module requires
+  bucket_prefix = trim(var.bucket_prefix, "/")
+}
+
+resource "aws_s3_bucket" "target" {
+  bucket = var.bucket_name
+}
+
+data "aws_s3_bucket" "target" {
+  bucket = var.create_bucket ? aws_s3_bucket.target.id : var.bucket_name
+}
+
+resource "aws_s3_object" "test" {
+  bucket = data.aws_s3_bucket.target.id #
+  key    = "${local.bucket_prefix}/test.txt"
+  source = "${path.module}/test.txt"
+}
