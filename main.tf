3f91dd9b5d73f4204d8f5646bbe89b86c5640ef1
Jamison Blanchard <jamison.p.blanchard@gmail.com>
2023-01-19 09:58:50 -0800

    Use etags to avoid unecessary updates to objects

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 locals {
   # Side step the leading and following slash issue by removing the provided
   # ones and adding them in the format the module requires
-  bucket_prefix = trim(var.bucket_prefix, "/")
+  bucket_prefix  = trim(var.bucket_prefix, "/")
+  test_file_path = "${path.module}/test.txt"
 }
 
 resource "aws_s3_bucket" "target" {
@@ -18,5 +19,8 @@ data "aws_s3_bucket" "target" {
 resource "aws_s3_object" "test" {
   bucket = data.aws_s3_bucket.target.id #
   key    = "${local.bucket_prefix}/test.txt"
-  source = "${path.module}/test.txt"
+  source = local.test_file_path
+  etag   = filemd5(local.test_file_path)
+
+  force_destroy = true
 }
